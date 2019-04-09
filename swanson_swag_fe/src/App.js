5200413cbba36ad5d262f28361350792eeef65e9
Chad <chad.zuniga10@gmail.com>
2019-04-08 23:21:19 -0600

    small mid and large quote api calls are working

--- swanson_swag_fe/src/App.js
+++ swanson_swag_fe/src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      smallQuote: []
+      smallQuote: [],
+      midQuote: [],
+      largeQuote:[]
     }
   }
   
@@ -15,6 +17,7 @@ class App extends Component {
 
   //trying to use a callback function to keep getting a new quote until condition is met
   smallQuote(){
+    this.setState({smallQuote: []});
     const callback = () => {
       getQuote()
         .then( res => {
@@ -31,12 +34,52 @@ class App extends Component {
     callback();
   }
 
+  midQuote(){
+    this.setState({midQuote: []});
+    const callback = () => {
+      getQuote()
+        .then( res => {
+          let dat = res.data[0].toString();
+          let q = dat.split(' ');
+          //condition
+          if(q.length < 4 || q.length >= 12){
+            callback();
+          }else{
+            this.setState({ midQuote: res.data[0] }); 
+          }
+        })
+    }
+    callback();
+  }
+
+  largeQuote(){
+    this.setState({largeQuote: []});
+    const callback = () => {
+      getQuote()
+        .then( res => {
+          let dat = res.data[0].toString();
+          let q = dat.split(' ');
+          //condition
+          if(q.length < 12){
+            callback();
+          }else{
+            this.setState({ largeQuote: res.data[0] }); 
+          }
+        })
+    }
+    callback();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <button onClick={(e) => this.smallQuote(e)}>small quote</button>
         {this.state.smallQuote}
+        <button onClick={(e) => this.midQuote(e)}>mid quote</button>
+        {this.state.midQuote}
+        <button onClick={(e) => this.largeQuote(e)}>large quote</button>
+        {this.state.largeQuote}
         </header>
       </div>
     );
